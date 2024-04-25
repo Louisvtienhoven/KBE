ba8f3e6ce727f547ae188803c2f018a9cedef0b7
Louisvtienhoven <l.j.m.vantienhoven@student.tudelft.nl>
2024-04-25 14:52:55 +0200

    small changes for channels

--- fuselage/EWIS.py
+++ fuselage/EWIS.py
@@ -4,57 +4,73 @@ from parapy.core import *
 from fuselage.channel import ChannelY, ChannelZ, ChannelX, ChannelSweep, ChannelVtail
 
 class Ewis(GeomBase):
-    @Part
-    def channel1(self):
-        return ChannelX(ch_radius=0.2, position=translate(self.position, 'x', 5, 'y', 1, 'z', -1), color="Blue")
+
+    lower_channel_zposition = Input(-1)
+    upper_channel_zposition = Input(1)
+    channels_ypostion = Input(1)
 
     @Part
-    def channel2(self):
-        return ChannelX(ch_radius=0.2, position=translate(self.position, 'x', 5, 'y', -1, 'z', -1), color="Blue")
+    def lower_channel(self):
+        return ChannelX(ch_radius=0.2, position=translate(self.position, 'x', 5, 'y', self.channels_ypostion, 'z', self.lower_channel_zposition), color="Blue")
 
     @Part
-    def channel3(self):
-        return ChannelX(ch_radius=.1, position=translate(self.position, 'x', 5, 'y', 1, 'z', 1), color="Blue", ch_length=35.2)
+    def lower_channel2(self):
+        return MirroredShape(shape_in=self.lower_channel,
+                             reference_point=self.position,
+                             # Two vectors to define the mirror plane
+                             vector1=self.position.Vz,
+                             vector2=self.position.Vx,
+                             mesh_deflection=0.0001,
+                             color='Blue')
+    @Part
+    def upper_channel(self):
+        return ChannelX(ch_radius=.1, position=translate(self.position, 'x', 5, 'y', self.channels_ypostion, 'z', self.upper_channel_zposition), color="Blue", ch_length=35.2)
 
     @Part
-    def channel4(self):
-        return ChannelX(ch_radius=.1, position=translate(self.position, 'x', 5, 'y', -1, 'z', 1), color='Blue',ch_length=35.2)
+    def upper_channel2(self):
+        return MirroredShape(shape_in=self.upper_channel,
+                             reference_point=self.position,
+                             # Two vectors to define the mirror plane
+                             vector1=self.position.Vz,
+                             vector2=self.position.Vx,
+                             mesh_deflection=0.0001,
+                             color='Blue')
 
     @Part
-    def channel5(self):
-        return ChannelY(ch_radius=.1, position=translate(self.position, 'x', 7, 'y', -1, 'z', -1), color='Blue')
+    def connectorY1(self):
+        return ChannelY(ch_length=self.channels_ypostion*2,ch_radius=.1, position=translate(self.position, 'x', 7, 'y', -1*self.channels_ypostion, 'z', self.lower_channel_zposition), color='Blue')
 
     @Part
-    def channel6(self):
-        return ChannelY(ch_radius=.1, position=translate(self.position, 'x', 40, 'y', -1, 'z', 1), color='Blue')
+    def connectorY2(self):
+        return ChannelY(ch_length=self.channels_ypostion*2,ch_radius=.1, position=translate(self.position, 'x', 40, 'y', -1*self.channels_ypostion, 'z', self.upper_channel_zposition), color='Blue')
 
     @Part
-    def channel62(self):
-        return ChannelY(ch_radius=.1, position=translate(self.position, 'x', 36.5, 'y', -1, 'z', 1), color='Blue')
+    def connectorY3(self):
+        return ChannelY(ch_length=self.channels_ypostion*2,ch_radius=.1, position=translate(self.position, 'x', 36.5, 'y', -1*self.channels_ypostion, 'z', self.upper_channel_zposition), color='Blue')
 
     @Part
-    def channel7(self):
-        return ChannelZ(ch_radius=.1, position=translate(self.position, 'x', 37.7, 'y', -1, 'z', -1), color='Blue')
+    def connectorZ1(self):
+        return ChannelZ(ch_length=self.upper_channel_zposition - self.lower_channel_zposition, ch_radius=.1, position=translate(self.position, 'x', 37.7, 'y', -1, 'z', self.lower_channel_zposition), color='Blue')
 
     @Part
-    def channel8(self):
-        return ChannelZ(ch_radius=.1, position=translate(self.position, 'x', 7, 'y', 1, 'z', -1), color='Blue')
+    def connectorZ2(self):
+        return ChannelZ(ch_length=self.upper_channel_zposition - self.lower_channel_zposition,ch_radius=.1, position=translate(self.position, 'x', 7, 'y', 1, 'z', self.lower_channel_zposition), color='Blue')
 
     @Part
-    def channel9(self):
-        return ChannelZ(ch_radius=.1, position=translate(self.position, 'x', 7, 'y', -1, 'z', -1), color='Blue')
+    def connectorZ3(self):
+        return ChannelZ(ch_length=self.upper_channel_zposition - self.lower_channel_zposition,ch_radius=.1, position=translate(self.position, 'x', 7, 'y', -1, 'z', self.lower_channel_zposition), color='Blue')
 
     @Part
-    def channel10(self):
-        return ChannelZ(ch_radius=.1, position=translate(self.position, 'x', 37.7, 'y', 1, 'z', -1), color='Blue')
+    def connectorZ4(self):
+        return ChannelZ(ch_length=self.upper_channel_zposition - self.lower_channel_zposition,ch_radius=.1, position=translate(self.position, 'x', 37.7, 'y', 1, 'z', self.lower_channel_zposition), color='Blue')
 
     @Part
-    def channel11(self):
+    def wing_frontspar(self):
         return ChannelSweep(ch_radius=.07, position=translate(self.position, 'x', 17, 'y', 1, 'z', -1), color='Blue')
 
     @Part
-    def channel12(self):
-        return MirroredShape(shape_in=self.channel11,
+    def wing_frontspar2(self):
+        return MirroredShape(shape_in=self.wing_frontspar,
                              reference_point=self.position,
                              # Two vectors to define the mirror plane
                              vector1=self.position.Vz,
@@ -62,12 +78,12 @@ class Ewis(GeomBase):
                              mesh_deflection=0.0001,
                              color='Blue')
     @Part
-    def channel13(self):
+    def wing_aftspar(self):
         return ChannelSweep(ch_radius=.07, position=translate(self.position, 'x', 20.7, 'y', 1, 'z', -1.05), color='Blue',sweep_rad=1.03, dihedral=0.145, ch_length=10.5)
 
     @Part
-    def channel14(self):
-        return MirroredShape(shape_in=self.channel13,
+    def wing_aftspar2(self):
+        return MirroredShape(shape_in=self.wing_aftspar,
                              reference_point=self.position,
                              # Two vectors to define the mirror plane
                              vector1=self.position.Vz,
@@ -76,12 +92,12 @@ class Ewis(GeomBase):
                              color='Blue')
 
     @Part
-    def channel15(self):
+    def wing_connector(self):
         return ChannelX(ch_radius=0.06, position=translate(self.position, 'x', 21.1, 'y', 6, 'z', -0.6), color="Blue", ch_length=2.15)
 
     @Part
-    def channel16(self):
-        return MirroredShape(shape_in=self.channel15,
+    def wing_connector2(self):
+        return MirroredShape(shape_in=self.wing_connector,
                              reference_point=self.position,
                              # Two vectors to define the mirror plane
                              vector1=self.position.Vz,
@@ -90,7 +106,7 @@ class Ewis(GeomBase):
                              color='Blue')
 
     @Part
-    def channel17(self):
+    def vtail_frontspar(self):
         return ChannelVtail(ch_radius=.04,
                             position=translate(self.position, 'x', 36.5, 'y', 0, 'z', 1),
                             color='Blue',
@@ -98,7 +114,7 @@ class Ewis(GeomBase):
                             dihedral=0.,
                             ch_length=6.5)
     @Part
-    def channel18(self):
+    def vtail_aftspar(self):
         return ChannelVtail(ch_radius=.04,
                             position=translate(self.position, 'x', 40, 'y', 0, 'z', 1),
                             color='Blue',
@@ -107,7 +123,7 @@ class Ewis(GeomBase):
                             ch_length=5.5)
 
     @Part
-    def channel19(self):
+    def vtail_connector(self):
         return ChannelX(ch_radius=0.02,
                         position=translate(self.position, 'x',39.3,'y',0, 'z',4),
                         color='Blue',
