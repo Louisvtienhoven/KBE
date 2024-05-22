81104e98a5559b65223d93699e3285d719ae8818
Louisvtienhoven <l.j.m.vantienhoven@student.tudelft.nl>
2024-05-22 15:22:18 +0200

    improve code

--- wiring/channel_definitions.py
+++ wiring/channel_definitions.py
@@ -2,7 +2,127 @@ from parapy.geom import *
 from parapy.core import *
 from math import *
 
+from parapy.geom import *
+from parapy.core import *
+from math import *
 
+
+class ChannelBase(LoftedSolid):
+    """Base class for channel geometries, representing a loft through circles."""
+
+    ch_radius = Input(0.1)
+    ch_sections = Input([100] * 7)
+    ch_length = Input(2.0)
+    transparency = 0.6
+
+    axis = 'x'  # Default axis, should be overridden in derived classes
+
+    @Attribute
+    def section_radius(self):
+        """Section radius multiplied by the radius distribution through the length."""
+        return [i * self.ch_radius / 100.0 for i in self.ch_sections]
+
+    @Attribute
+    def section_length(self):
+        """Section length is determined by dividing the fuselage length by the number of fuselage sections."""
+        return self.ch_length / (len(self.ch_sections) - 1)
+
+    @Attribute
+    def rotation_axis(self):
+        """Rotation axis for the channel profile, to be defined in derived classes."""
+        raise NotImplementedError("Derived class must define rotation_axis")
+
+    @Attribute
+    def translation_vector(self):
+        """Translation vector for the channel profile, to be defined in derived classes."""
+        raise NotImplementedError("Derived class must define translation_vector")
+
+    @Part(in_tree=(__name__ == "__main__"))
+    def profiles(self):
+        return Circle(
+            quantify=len(self.ch_sections),
+            color="Black",
+            radius=self.section_radius[child.index],
+            position=translate(
+                self.position.rotate90(self.rotation_axis),
+                self.translation_vector,
+                child.index * self.section_length,
+            ),
+        )
+
+
+class ChannelX(ChannelBase):
+    axis = 'x'
+    ch_length = Input(32.0)
+
+    @Attribute
+    def rotation_axis(self):
+        return 'y'
+
+    @Attribute
+    def translation_vector(self):
+        return Vector(1, 0, 0)
+
+
+class ChannelY(ChannelBase):
+    axis = 'y'
+
+    @Attribute
+    def rotation_axis(self):
+        return 'x'
+
+    @Attribute
+    def translation_vector(self):
+        return Vector(0, 1, 0)
+
+
+class ChannelZ(ChannelBase):
+    axis = 'z'
+
+    @Attribute
+    def rotation_axis(self):
+        return 'z'
+
+    @Attribute
+    def translation_vector(self):
+        return Vector(0, 0, 1)
+
+
+class ChannelSweep(ChannelBase):
+    sweep_rad = Input(0.6283)
+    dihedral = Input(0.06)
+    ch_length = Input(12.0)
+
+    @Attribute
+    def rotation_axis(self):
+        return 'y'
+
+    @Attribute
+    def translation_vector(self):
+        return Vector(sin(self.sweep_rad), tan(self.sweep_rad), self.dihedral)
+
+
+class ChannelVtail(ChannelBase):
+    sweep_rad = Input(0.6283)
+    dihedral = Input(0.06)
+    ch_length = Input(12.0)
+
+    @Attribute
+    def rotation_axis(self):
+        return 'x'
+
+    @Attribute
+    def translation_vector(self):
+        return Vector(sin(self.sweep_rad), 0, tan(self.sweep_rad))
+
+
+if __name__ == "__main__":
+    from parapy.gui import display
+
+    obj = ChannelVtail()
+    display(obj)
+
+"""
 class ChannelX(
     LoftedSolid
 ):  # note the use of LoftedSolid as superclass. It means that every Fuselage instance /
@@ -357,3 +477,4 @@ if __name__ == "__main__":
 
     obj = ChannelVtail()
     display(obj)
+"""
